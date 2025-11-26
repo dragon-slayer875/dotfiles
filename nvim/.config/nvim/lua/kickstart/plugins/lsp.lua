3504720b1b3ad5651a05ec510b2364319beb604a
Rudraksh Tyagi <rudraksh875@gmail.com>
2025-11-26 17:29:07 +0530

    nvim: update LSP config
    Signed-off-by: Rudraksh Tyagi <rudraksh875@gmail.com>

--- nvim/.config/nvim/lua/kickstart/plugins/lsp.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lsp.lua
@@ -1,127 +1,129 @@
 return {
-  -- Main LSP Configuration
-  'neovim/nvim-lspconfig',
-  event = { 'BufReadPre', 'BufNewFile' },
-  dependencies = {
-    { 'mason-org/mason.nvim', opts = {} },
-    'mason-org/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    { 'j-hui/fidget.nvim', opts = {}, event = { 'BufReadPre' } },
-  },
-  config = function()
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-      callback = function(event)
-        local map = function(keys, func, desc, mode)
-          mode = mode or 'n'
-          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-        end
+	-- Main LSP Configuration
+	'neovim/nvim-lspconfig',
+	event = { 'BufReadPre', 'BufNewFile' },
+	dependencies = {
+		{ 'mason-org/mason.nvim', opts = {} },
+		'mason-org/mason-lspconfig.nvim',
+		'WhoIsSethDaniel/mason-tool-installer.nvim',
+		{ 'j-hui/fidget.nvim',    opts = {}, event = { 'BufReadPre' } },
+	},
+	config = function()
+		vim.api.nvim_create_autocmd('LspAttach', {
+			group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+			callback = function(event)
+				local map = function(keys, func, desc, mode)
+					mode = mode or 'n'
+					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+				end
 
-        map('<leader>lr', vim.lsp.buf.rename, 'Rename current symbol')
+				map('<leader>lr', vim.lsp.buf.rename, 'Rename current symbol')
 
-        map('<leader>la', vim.lsp.buf.code_action, 'LSP code action', { 'n', 'x' })
+				map('<leader>la', vim.lsp.buf.code_action, 'LSP code action', { 'n', 'x' })
 
-        map('<leader>fr', "<cmd>Pick lsp scope='references'<CR>", 'Find references')
+				map('<leader>ld', vim.diagnostic.open_float, 'Show Hover diagnostics')
 
-        map('<leader>fi', "<cmd>Pick lsp scope='implementation'<CR>", '[G]oto [I]mplementation')
+				map('<leader>fr', "<cmd>Pick lsp scope='references'<CR>", 'Find references')
 
-        --  To jump back, press <C-t>.
-        map('gd', vim.lsp.buf.definition, 'Goto Definition')
+				map('<leader>fi', "<cmd>Pick lsp scope='implementation'<CR>", '[G]oto [I]mplementation')
 
-        map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
+				--  To jump back, press <C-t>.
+				map('gd', vim.lsp.buf.definition, 'Goto Definition')
 
-        map('<leader>ls', "<cmd>Pick lsp scope='document_symbol'<CR>", 'Document Symbols')
+				map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
 
-        map('<leader>lw', "<cmd>Pick lsp scope='workspace_symbol'<CR>", 'Workspace Symbols')
+				map('<leader>ls', "<cmd>Pick lsp scope='document_symbol'<CR>", 'Document Symbols')
 
-        map('gt', '<cmd>Pick lsp scope=type_definition<CR>', '[G]oto [T]ype Definition')
+				map('<leader>lw', "<cmd>Pick lsp scope='workspace_symbol'<CR>", 'Workspace Symbols')
 
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-          local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.document_highlight,
-          })
+				map('gt', '<cmd>Pick lsp scope=type_definition<CR>', '[G]oto [T]ype Definition')
 
-          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.clear_references,
-          })
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+					local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+					vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.document_highlight,
+					})
 
-          vim.api.nvim_create_autocmd('LspDetach', {
-            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-            callback = function(event2)
-              vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-            end,
-          })
-        end
+					vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.clear_references,
+					})
 
-        -- The following code creates a keymap to toggle inlay hints in your
-        -- code, if the language server you are using supports them
-        --
-        -- if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-        --   map('<leader>th', function()
-        --     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-        --   end, '[T]oggle Inlay [H]ints')
-        -- end
-      end,
-    })
+					vim.api.nvim_create_autocmd('LspDetach', {
+						group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+						callback = function(event2)
+							vim.lsp.buf.clear_references()
+							vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+						end,
+					})
+				end
 
-    vim.diagnostic.config {
-      virtual_lines = { current_line = true },
-      severity_sort = true,
-      float = { border = 'rounded', source = 'if_many' },
-      underline = false,
-      signs = vim.g.have_nerd_font and {
-        text = {
-          [vim.diagnostic.severity.ERROR] = '󰅚 ',
-          [vim.diagnostic.severity.WARN] = '󰀪 ',
-          [vim.diagnostic.severity.INFO] = '󰋽 ',
-          [vim.diagnostic.severity.HINT] = '󰌶 ',
-        },
-      } or {},
-    }
+				-- The following code creates a keymap to toggle inlay hints in your
+				-- code, if the language server you are using supports them
+				--
+				-- if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+				--   map('<leader>th', function()
+				--     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+				--   end, '[T]oggle Inlay [H]ints')
+				-- end
+			end,
+		})
 
-    --  Add any additional override configuration in the following tables. Available keys are:
-    --  - cmd (table): Override the default command used to start the server
-    --  - filetypes (table): Override the default list of associated filetypes for the server
-    --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --  - settings (table): Override the default settings passed when initializing the server.
-    --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-    local servers = {
-      gopls = {},
-      rust_analyzer = {},
-      ts_ls = {},
-      lua_ls = {
-        -- filetypes = { ... },
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = 'Replace',
-            },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
-          },
-        },
-      },
-    }
+		vim.diagnostic.config {
+			virtual_lines = { current_line = true },
+			severity_sort = true,
+			float = { border = 'rounded', source = 'if_many' },
+			underline = false,
+			signs = vim.g.have_nerd_font and {
+				text = {
+					[vim.diagnostic.severity.ERROR] = '󰅚 ',
+					[vim.diagnostic.severity.WARN] = '󰀪 ',
+					[vim.diagnostic.severity.INFO] = '󰋽 ',
+					[vim.diagnostic.severity.HINT] = '󰌶 ',
+				},
+			} or {},
+		}
 
-    local ensure_installed = vim.tbl_keys(servers or {})
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+		--  Add any additional override configuration in the following tables. Available keys are:
+		--  - cmd (table): Override the default command used to start the server
+		--  - filetypes (table): Override the default list of associated filetypes for the server
+		--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+		--  - settings (table): Override the default settings passed when initializing the server.
+		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+		local servers = {
+			gopls = {},
+			rust_analyzer = {},
+			ts_ls = {},
+			lua_ls = {
+				-- filetypes = { ... },
+				settings = {
+					Lua = {
+						completion = {
+							callSnippet = 'Replace',
+						},
+						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+						-- diagnostics = { disable = { 'missing-fields' } },
+					},
+				},
+			},
+		}
 
-    require('mason-lspconfig').setup {
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_installation = false,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
-  end,
+		local ensure_installed = vim.tbl_keys(servers or {})
+		require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+		require('mason-lspconfig').setup {
+			ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+			automatic_installation = false,
+			handlers = {
+				function(server_name)
+					local server = servers[server_name] or {}
+					require('lspconfig')[server_name].setup(server)
+				end,
+			},
+		}
+	end,
 }
