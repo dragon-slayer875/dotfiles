f39f60a4eef87c7cacd3e49dc67f6deb8b8c8fb5
Rudraksh Tyagi <rudraksh875@gmail.com>
2026-01-26 20:15:22 +0530

    nvim: replace neo-tree with fyler
    Signed-off-by: Rudraksh Tyagi <rudraksh875@gmail.com>

---
+++ nvim/.config/nvim/lua/plugins/fyler.lua
@@ -0,0 +1,24 @@
+return {
+	"A7Lavinraj/fyler.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	lazy = false,
+	opts = {
+		integrations = {
+			icon = "nvim_web_devicons",
+		},
+		views = {
+			finder = {
+				default_explorer = true,
+			}
+		},
+	},
+	keys = {
+		{
+			"<leader>e",
+			function()
+				require("fyler").toggle({ kind = "split_right_most" })
+			end,
+			desc = "Toggle File Explorer"
+		},
+	}
+}
