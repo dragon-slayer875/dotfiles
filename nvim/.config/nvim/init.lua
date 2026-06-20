544bd9e781de43ebcdab93fe7030ca41463ee756
Rudraksh Tyagi <rudraksh875@gmail.com>
2026-06-20 14:58:15 +0530

    nvim: format config
    Signed-off-by: Rudraksh Tyagi <rudraksh875@gmail.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
 -- Set <space> as the leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
@@ -16,26 +16,21 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
--- Enable folding on according to treesitter evaluation
-require('folds').setup()
-
-vim.opt.fillchars = { eob = ' ', fold = ' ' }
+vim.opt.fillchars = { eob = " ", fold = " " }
 vim.opt.termguicolors = true
 
 -- Set statusline appearance condition and command line height
 vim.o.laststatus = 3
 vim.o.cmdheight = 1
-vim.o.showcmdloc = 'statusline'
+vim.o.showcmdloc = "statusline"
 
 -- Sync clipboard between OS and Neovim.
-vim.schedule(function()
-	vim.o.clipboard = 'unnamedplus'
-end)
+vim.schedule(function() vim.o.clipboard = "unnamedplus" end)
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -48,7 +43,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 
 -- Decrease update time
 vim.o.updatetime = 250
@@ -66,10 +61,10 @@ vim.o.splitbelow = true
 --   See `:help lua-options`
 --   and `:help lua-options-guide`
 vim.o.list = true
-vim.opt.listchars = { tab = '  ', trail = ' ', nbsp = '␣' }
+vim.opt.listchars = { tab = "  ", trail = " ", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
+vim.o.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.o.cursorline = true
@@ -85,60 +80,65 @@ vim.o.confirm = true
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '|', '<cmd>vsplit<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "|", "<cmd>vsplit<CR>")
 
 -- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('n', '<leader>q', '<cmd>confirm quit<CR>', { desc = 'Close Window' })
-vim.keymap.set('n', '<leader>w', '<cmd>write<CR>', { desc = 'Save changes' })
-vim.keymap.set('n', '<leader>c', '<cmd>bp|bd#<CR>', { desc = 'Close Buffer' })
-vim.keymap.set('n', '<leader>tt', '<cmd>split<CR><cmd>terminal<CR>', { desc = 'Open [t]erminal with horizontal split' })
-vim.keymap.set('n', '<leader>tT', '<cmd>vsplit<CR><cmd>terminal<CR>', { desc = 'Open [t]erminal with vertical split' })
+vim.keymap.set("n", "<leader>q", "<cmd>confirm quit<CR>", { desc = "Close Window" })
+vim.keymap.set("n", "<leader>w", "<cmd>write<CR>", { desc = "Save changes" })
+vim.keymap.set("n", "<leader>c", "<cmd>bp|bd#<CR>", { desc = "Close Buffer" })
+vim.keymap.set("n", "<leader>tt", "<cmd>split<CR><cmd>terminal<CR>", { desc = "Open [t]erminal with horizontal split" })
+vim.keymap.set("n", "<leader>tT", "<cmd>vsplit<CR><cmd>terminal<CR>", { desc = "Open [t]erminal with vertical split" })
 
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- TIP: Disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
-vim.keymap.set('n', '<C-S-h>', '<C-w>H', { desc = 'Move window to the left' })
-vim.keymap.set('n', '<C-S-l>', '<C-w>L', { desc = 'Move window to the right' })
-vim.keymap.set('n', '<C-S-j>', '<C-w>J', { desc = 'Move window to the lower' })
-vim.keymap.set('n', '<C-S-k>', '<C-w>K', { desc = 'Move window to the upper' })
+vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
 -- Mark motion wrappers
-vim.keymap.set('n', 'gm', '``', { desc = 'Go to last mark' })
+vim.keymap.set("n", "gm", "``", { desc = "Go to last mark" })
 
 -- Commenting wrappers
-vim.keymap.set('n', '<leader>/', 'gcc', { desc = 'Toggle comment for lines', remap = true })
-vim.keymap.set('v', '<leader>/', 'gc', { desc = 'Toggle comment for selection', remap = true })
+vim.keymap.set("n", "<leader>/", "gcc", { desc = "Toggle comment for lines", remap = true })
+vim.keymap.set("v", "<leader>/", "gc", { desc = "Toggle comment for selection", remap = true })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-	callback = function()
-		vim.hl.on_yank()
-	end,
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "<filetype>" },
+  callback = function() vim.treesitter.start() end,
 })
 
-require 'lazy-init'
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function() vim.hl.on_yank() end,
+})
 
-vim.cmd 'colorscheme onedark'
+require "lazy-init"
+
+vim.cmd "colorscheme onedark"
+
+-- Enable folding on according to treesitter evaluation
+require("folds").setup()
